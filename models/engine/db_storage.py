14d2298a17f62b0a7077344462b60dc5bd37ee40
maxnchief <logandogan@gmail.com>
2024-10-26 14:07:28 -0500

    creates a database storage class connected to all other models

---
+++ models/engine/db_storage.py
@@ -0,0 +1,50 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        user = os.getenv('HBNB_MYSQL_USER')
+        password = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST', 'localhost')
+        database = os.getenv('HBNB_MYSQL_DB')
+        self.__engine = create_engine(f'mysql+mysqldb://{user}:{password}@{host}/{database}', pool_pre_ping=True)
+
+        if os.getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        if cls:
+            objects = self.__session.query(cls).all()
+        else:
+            objects = []
+            for class_ in [User, State, City, Amenity, Place, Review]:
+                objects.extend(self.__session.query(class_).all())
+        
+        return {f"{type(obj).__name__}.{obj.id}": obj for obj in objects}
+
+    def new(self, obj):
+        self.__session.add(obj)
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
