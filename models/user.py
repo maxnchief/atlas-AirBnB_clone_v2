2f098888ccc0fe12989972a49027438ccbfbb0ac
maxnchief <logandogan@gmail.com>
2024-10-29 13:43:16 -0500

    transformed into an if statement to allow for empty data in the case of the user not existing in storage

--- models/user.py
+++ models/user.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+import models
+from models.base_model import BaseModel
 from sqlalchemy import Column, String
-from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from models.place import Place
 
 
-class User(BaseModel, Base, __tablename__, email, password, first_name, last_name):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user", cascade="all, delete, delete-orphan")
-    reviews = relationship("Review", backref="user")
+class User(BaseModel, Base):
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
