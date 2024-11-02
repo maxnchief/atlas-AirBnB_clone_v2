17cde4474a155c1ab0cd2c1afb46e2cffc955f2c
maxnchief <logandogan@gmail.com>
2024-11-02 18:11:06 -0500

    PEP8 validation fixes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,6 +3,7 @@
 This module sets up a simple Flask web application with a single route.
 """
 
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
