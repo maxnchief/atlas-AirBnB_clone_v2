d5cca274dcadea6e0cde3bf4b58c54abae2cf20c
maxnchief <logandogan@gmail.com>
2024-11-02 18:09:53 -0500

    reduced the documentation length

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,23 +1,8 @@
 #!/usr/bin/python3
 """
 This module sets up a simple Flask web application with a single route.
-
-Routes:
-    /: Returns the string "Hello HBNB!" when accessed.
-
-Usage:
-    Run this script to start the Flask web application. The application will be accessible
-    at http://0.0.0.0:5000/.
-
-Example:
-    $ python3 0-hello_route.py
-
-Dependencies:
-    Flask: A micro web framework for Python.
-
-Functions:
-    hello_hbnb(): Returns a greeting string "Hello HBNB!" when the root URL is accessed.
 """
+
 from flask import Flask
 
 app = Flask(__name__)
