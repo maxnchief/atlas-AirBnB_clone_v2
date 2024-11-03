9b06719ebd71691a2638913807d9a1f7fd9d5bd2
maxnchief <logandogan@gmail.com>
2024-11-03 17:57:01 -0600

    Add module docstring to cities_by_states Flask application

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+"""
+This module defines a Flask web application that displays a list of cities by states.
+
+"""
 
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-
-
 app = Flask(__name__)
+
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
@@ -14,9 +17,11 @@ def cities_by_states():
     states = sorted(states, key=lambda state: state.name)
     return render_template('8-cities_by_states.html', states=states)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
