bfcb4e86740088b53966c9ac2be0e54f8802f012
maxnchief <logandogan@gmail.com>
2024-11-03 17:39:38 -0600

    updated close methods to file storage and db storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -53,3 +53,7 @@ class FileStorage:
                 key: val for key, val in FileStorage.__objects.items()
                 if isinstance(val, cls)
             }
+            
+    def close(self):
+        """Call reload() method for deserializing the JSON file to objects"""
+        self.reload()
