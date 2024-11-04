a2d768c608758022ca2690e9f1a485ffd0e9e889
maxnchief <logandogan@gmail.com>
2024-11-03 18:02:03 -0600

    gotem?

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """
 This module defines a Flask web application that displays a list of cities by states.
+
+Routes:
+    /cities_by_states: Displays a HTML page with a list of all State objects and their related City objects.
+
+Functions:
+    cities_by_states(): Retrieves all State objects from storage, sorts them by name, and renders them in a template.
+    teardown_db(exception): Closes the storage session after each request.
+
+Usage:
+    Run this script to start the Flask web application. The application listens on all public IPs on port 5000.
 """
+
 
 from flask import Flask, render_template
 from models import storage
