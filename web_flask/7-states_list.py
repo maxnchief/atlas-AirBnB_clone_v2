d8578f1d9fe269feb075584fa137595508881a8c
maxnchief <logandogan@gmail.com>
2024-11-05 20:28:41 -0600

    fledged out the document

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+This module defines a Flask web application that serves a list of states.
+
+Routes:
+    /states_list: Displays an HTML page with the states listed in alphabetical order.
+
+Functions:
+    states_list: Retrieves the list of states from the storage and renders the HTML template.
+    teardown_db: Closes the storage on teardown of the Flask app context.
+
+Usage:
+    Run this script to start the Flask web application. The app will be accessible
+    at http://0.0.0.0:5000/states_list.
 """
 
 from flask import Flask, render_template
