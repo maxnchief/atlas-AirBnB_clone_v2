568f4966e11f58411ce96747b2ee964399199d9a
maxnchief <logandogan@gmail.com>
2024-11-14 23:33:29 -0600

    refactor: reorganize storage import statements based on storage type

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,12 +1,12 @@
 import os
-from models.engine.db_storage import DBStorage
-from models.engine.file_storage import FileStorage
 
 storage_type = os.getenv('HBNB_TYPE_STORAGE')
 
 if storage_type == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
 storage.reload()
